97efc5045c0bcb91fb0c7adb998ada3356e292b5
Usiel Amaral <usiel@usis.com.br>
2022-08-30 16:25:34 -0300

    Add Tailwind CSS

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,24 +3,42 @@
 @section('title', 'Listagem dos usuários')
 
 @section('content')
-    <h1>
+    <h1 class="text-2x1 font-semibold leading-tigh py-2">
         Listagem de usuários
-        (<a href="{{ route('users.create') }}">+</a>)
+        (<a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>)
     </h1>
 
     <form action="{{ route('users.index') }}" method="get">
         <input type="text" name="search" placeholder="Pesquisar">
-        <button>Pesquisar</button>
+        <button class="shadow bg-purple-500 hover:bg-purple rounded-full px-4">Pesquisar</button>
     </form>
 
-    <ul>
-        @foreach ($users as $user)
-            <li>
-                {{ $user->name}} -
-                {{ $user->email}}
-                | <a href="{{ route('users.edit', $user->id)}}">Editar</a>
-                | <a href="{{ route('users.show', $user->id)}}">Detalhes</a>
-            </li>
-        @endforeach
-    </ul>
+    <table class="min-w-full leading-normal shadow-md rounded-lg overflow-hidden">
+        <thead>
+            <tr>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                    Nome
+                </th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                    E-mail
+                </th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                    Editar
+                </th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                    Detalhes
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td class="px-5 py-5 border-b border-gray-200">{{ $user->name}}</td>
+                    <td class="px-5 py-5 border-b border-gray-200">{{ $user->email}}</td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-gray-100 rounded-full text-center"><a href="{{ route('users.edit', $user->id)}}">Editar</a></td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-gray-100 rounded-full text-center"><a href="{{ route('users.show', $user->id)}}">Detalhes</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
